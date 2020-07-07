11a1590f4c4713f72e2d94b23c9b64895b4c5ebd
Valerii Druzhynin <valerii.druzhynin@actionmediamtl.com>
2020-07-07 10:25:56 -0500

    MT-544 Made some fixes for DataTable.

--- routes/affiliates.js
+++ routes/affiliates.js
@@ -10,12 +10,10 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-
     const affiliatesData = await new Affiliates().findAllAffiliates(req.body.start, req.body.length)
     const itemCount = affiliatesData.count;
-    const data = affiliatesData.rows
+    const data = affiliatesData.rows;
 
-    console.log(affiliatesData.rows[0])
     res.json({
         draw: 1,
         recordsTotal: itemCount,
