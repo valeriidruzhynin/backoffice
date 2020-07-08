0cd42896f40d48ae19393b9d407d0c66c75042b6
Valerii Druzhynin <valerii.druzhynin@actionmediamtl.com>
2020-07-08 05:01:16 -0500

    MT-544 Added displaying affiliate type.

--- controllers/affiliates.js
+++ controllers/affiliates.js
@@ -5,7 +5,7 @@ class Affiliates {
         return await AffiliatesModel.findAndCountAll({
             offset: +offset,
             limit: +limit,
-            attributes: ['id', 'email', 'first_name', 'last_name', 'status', 'employee_id', 'account_mgr_id', 'account_tier'],
+            attributes: ['id', 'email', 'first_name', 'last_name', 'status', 'affiliate_type', 'employee_id', 'account_mgr_id', 'account_tier'],
             order: [
                 ['id', 'DESC']
             ],
